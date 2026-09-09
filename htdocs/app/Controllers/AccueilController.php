e47694b80b96a67970e2aca111564d841fea2348
Ikar <lucas.lbruno2007@gmail.com>
2026-09-09 16:22:46 +0400

    Enhance index method to include additional data

--- htdocs/app/Controllers/AccueilController.php
+++ htdocs/app/Controllers/AccueilController.php
@@ -3,11 +3,27 @@ class AccueilController extends Controller
 {
     public function index(): void
     {
-        $epreuvesAVenir = Epreuve::aVenir();
+        $villes    = Ville::toutes();
+        $sports    = Sport::tous();
+        $equipes   = Equipe::toutes();
+        $epreuves  = Epreuve::toutes();
+
+        $epreuvesParStatut = ['a_venir' => 0, 'en_cours' => 0, 'terminee' => 0];
+        foreach ($epreuves as $epreuve) {
+            $statut = $epreuve['statut'] ?? null;
+            if (isset($epreuvesParStatut[$statut])) {
+                $epreuvesParStatut[$statut]++;
+            }
+        }
 
         $this->afficher('accueil/index', [
-            'titre'          => 'Accueil',
-            'epreuvesAVenir' => $epreuvesAVenir,
+            'titre'             => 'Accueil',
+            'nombreVilles'      => count($villes),
+            'nombreSports'      => count($sports),
+            'nombreEquipes'     => count($equipes),
+            'nombreEpreuves'    => count($epreuves),
+            'epreuvesParStatut' => $epreuvesParStatut,
+            'epreuvesAVenir'    => Epreuve::aVenir(),
         ]);
     }
 }
